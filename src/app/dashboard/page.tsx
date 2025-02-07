c10007f05bb41a687e452a2684ed446c36e87e9c
rahul00096 <88938719+rahul00096@users.noreply.github.com>
2025-02-07 18:54:31 +0530

    feat:make header and sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
-const page = () => {
-  return <div>this home page</div>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <div className="flex">
+      {/* Sidebar */}
+      <Sidebar isCollapsed={isCollapsed} />
+
+      {/* Main Content */}
+      <div className="flex-1">
+        {/* Header */}
+        <Header toggleSidebar={() => setIsCollapsed(!isCollapsed)} />
+
+        {/* Page Content */}
+        <main className="p-4">{children}</main>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Layout;
