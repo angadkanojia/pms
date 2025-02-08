19e438226237a447e238ad886fd613f90f6e97d5
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2025-02-08 14:24:28 +0530

    feat: classname remove

--- src/common/_components/layout/PageLayout.tsx
+++ src/common/_components/layout/PageLayout.tsx
@@ -18,7 +18,7 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
         {/* Header */}
         <Header toggleSidebar={() => setIsCollapsed(!isCollapsed)} />
         {/* Page Content */}
-        <main className="p-4">{children}</main>
+        <main>{children}</main>
        
       </div>
     </div>
