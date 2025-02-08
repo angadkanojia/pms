88d31fada489ca397c161d3fa3e2991e245104d0
rahul00096 <88938719+rahul00096@users.noreply.github.com>
2025-02-08 14:13:07 +0530

    feat:dashboard page show text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,19 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <Sidebar isCollapsed={isCollapsed} />
-
-      {/* Main Content */}
-      <div className="flex-1">
-        {/* Header */}
-        <Header toggleSidebar={() => setIsCollapsed(!isCollapsed)} />
-
-        {/* Page Content */}
-        <main className="p-4">{children}</main>
-      </div>
-    </div>
+    <h1>dashboard</h1>
   )
 }
 
