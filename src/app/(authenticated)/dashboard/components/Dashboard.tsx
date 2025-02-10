e1918c5b53e6bdae25c0354ed4e282a408aa0a8f
rahul00096 <88938719+rahul00096@users.noreply.github.com>
2025-02-10 18:46:44 +0530

    feat:make proper filter

--- src/app/(authenticated)/dashboard/components/Dashboard.tsx
+++ src/app/(authenticated)/dashboard/components/Dashboard.tsx
@@ -1,38 +1,41 @@
-"use client"
-//import { authOptions } from "../../lib/authOptions";
-import React from "react";
-import { useState } from "react";
+"use client";
+import React, { useState } from "react";
 import DashboardCard from "./DashboardCard";
-export default  function Dashboard() {
- const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = (event:any) => {
+export default function Dashboard() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterStatus, setFilterStatus] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-
-  
   return (
     <div className="bg-gray-200">
       <div className="p-5">
-        <div className="bg-white flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 shadow-md rounded-md py-5 px-10 ">
-          <h1 className="text-2xl font-bold">Dashbaord</h1>
+        <div className="bg-white flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 shadow-md rounded-md py-5 px-10">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4 w-full sm:w-auto">
             <input
               type="text"
               placeholder="Search for Document"
               value={searchTerm}
               onChange={handleSearch}
-              className="border p-2  rounded-md w-full sm:w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="border p-2 rounded-md w-full sm:w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <button className="bg-primary text-white px-4 py-3 rounded-lg font-semibold hover:bg-blue-600 w-full sm:w-auto">
               Create Template
             </button>
           </div>
         </div>
-        <DashboardCard searchTerm={searchTerm}/>
+
+        {/* Pass all required props to DashboardCard */}
+        <DashboardCard 
+          searchTerm={searchTerm} 
+          filterStatus={filterStatus} 
+          setFilterStatus={setFilterStatus} 
+        />
       </div>
-     
     </div>
   );
 }
