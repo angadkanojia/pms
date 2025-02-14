634305de296237627c9b6d8969567197b95c2d83
rahul00096 <88938719+rahul00096@users.noreply.github.com>
2025-02-14 18:10:43 +0530

    feat:create section builder page

---
+++ src/app/(authenticated)/section-builder/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Sectionbuilder from './_components/Sectionbuilder'
+
+const page = () => {
+  return (
+    <div>
+      <Sectionbuilder/>
+    </div>
+  )
+}
+
+export default page
