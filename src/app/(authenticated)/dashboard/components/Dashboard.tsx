e8b44f57e4eb57d976232cfb3774771bd3804b27
DK <deepakk@geeconglobal.com>
2025-02-16 20:27:24 +0530

    test

--- src/app/(authenticated)/dashboard/components/Dashboard.tsx
+++ src/app/(authenticated)/dashboard/components/Dashboard.tsx
@@ -12,17 +12,17 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="bg-white flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 shadow-md rounded-md py-5 px-10">
+      <div className="flex flex-col items-center justify-between space-y-4 rounded-md bg-white px-10 py-5 shadow-md sm:flex-row sm:space-y-0">
         <h1 className="text-2xl font-bold">Dashboard</h1>
-        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4 w-full sm:w-auto">
+        <div className="flex w-full flex-col items-center space-y-2 sm:w-auto sm:flex-row sm:space-x-4 sm:space-y-0">
           <input
             type="text"
             placeholder="Search for Document"
             value={searchTerm}
             onChange={handleSearch}
-            className="border p-2 rounded-md w-full sm:w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full rounded-md border p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:w-64"
           />
-          <button className="bg-primary text-white px-4 py-3 rounded-lg font-semibold hover:bg-blue-600 w-full sm:w-auto">
+          <button className="w-full rounded-lg bg-primary px-4 py-3 font-semibold text-white hover:bg-blue-600 sm:w-auto">
             Create Template
           </button>
         </div>
@@ -33,6 +33,6 @@ export default function Dashboard() {
         filterStatus={filterStatus}
         setFilterStatus={setFilterStatus}
       />
-    </>
+    
   );
 }
